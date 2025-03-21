9719dfd3476e94a7cd4cf0de1fdf928c9a6d66e3
nxrmxt <86514842+nxrmxt@users.noreply.github.com>
2025-03-21 20:37:29 +0530

    Uploading Files Manually

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ["images.unsplash.com", "res.cloudinary.com"], // Allow Unsplash images
+    unoptimized: true, 
+  },
+  output: "export"
+};
+
+export default nextConfig;
